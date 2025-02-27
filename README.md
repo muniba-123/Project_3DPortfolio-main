d6864bf91e6cc00622516b914ddd3e259ecf1069
Muniba Hina <muniba.fjwu@gmail.com>
2025-02-27 12:43:28 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,88 @@
+# Project 3D - Portfolio
+
+This was my first project using Vite. Initially made using tutorial of [JavaScipt Mastery](https://youtu.be/0fYi8SGA20k?feature=shared).
+
+Work done on it other than the tutorial:-
+
+- Removed unused CSS.
+- Removed Extra Animations.
+- Made UI Faster.
+- Added a Resume Button (Where you can put a link to your resume).
+- Enhanced Visibility and solved Responsive UI Problems.
+
+This Project is hosted in Netlify : [3D Portfolio](https://portfoliobyompatel.netlify.app/)
+
+## :star: Give A Star
+
+You can also give this repository a star to show more people and they can use this repository.
+
+# Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+Prerequisites
+Before you start, make sure you have the following software installed on your computer:
+
+[![My Skills](https://skillicons.dev/icons?i=nodejs)](https://skillicons.dev)
+
+## ℹ️ How to use this Repository?
+
+1. Clone the repository to your local machine
+
+```bash
+  git clone https://github.com/muniba-123/Project_3DPortfolio.git
+
+```
+
+2. Navigate to the project directory
+
+```bash
+  cd Project_3DPortfolio
+```
+
+3. Install the necessary dependencies
+
+```bash
+  npm install
+```
+
+4. Start the development server
+
+```bash
+  npm run dev
+```
+
+The website should now be up and running on http://localhost:3000.
+
+## 🏠 Built With:
+
+[![My Skills](https://skillicons.dev/icons?i=vscode,react,nextjs,threejs,tailwind,netlify)](https://skillicons.dev)
+
+## 🛠 Skills
+
+[![My Skills](https://skillicons.dev/icons?i=html,css,js,ts,react,nextjs,tailwind,threejs)](https://skillicons.dev)
+
+## 🚀 About Me
+
+Specializations:
+
+- UI/UX Design.
+- Web Development
+
+## 🙋‍♂️ Connect With Me
+
+[<img src="https://skillicons.dev/icons?i=github" />](https://github.com/muniba-123)&nbsp;
+[<img src="https://skillicons.dev/icons?i=linkedin" />](https://www.linkedin.com/in/muniba-hina-22342b111/)&nbsp;
+[<img src="https://skillicons.dev/icons?i=devto" />](https://portfoliobyom.netlify.app/)
+
+## 🗒️ Authors
+
+- Muniba Hina
+
+<p align="left">
+  <a href="https://skillicons.dev">
+    <a href="https://github.com/muniba-123">
+      <img src="https://skillicons.dev/icons?i=github" />
+    </a>
+  </a>
+</p>
---
+++ app/constants/index.ts
@@ -0,0 +1,265 @@
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "UI/UX Developer",
+    icon: "/backend.webp",
+  },
+  {
+    title: "Full Stack Web Developer",
+    icon: "/web.webp",
+  },
+  {
+    title: "Software Engineer",
+    icon: "/creator.webp",
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: "/tech/html.webp",
+  },
+  {
+    name: "CSS 3",
+    icon: "/tech/css.webp",
+  },
+  {
+    name: "JavaScript",
+    icon: "/tech/javascript.webp",
+  },
+  {
+    name: "TypeScript",
+    icon: "/tech/typescript.webp",
+  },
+  {
+    name: "React JS",
+    icon: "/tech/reactjs.webp",
+  },
+  {
+    name: "Next.JS",
+    icon: "/tech/nextjs.svg",
+  },
+  {
+    name: "Redux Toolkit",
+    icon: "/tech/redux.webp",
+  },
+  {
+    name: "Tailwind CSS",
+    icon: "/tech/tailwind.webp",
+  },
+  {
+    name: "Three JS",
+    icon: "/tech/threejs.webp",
+  },
+  {
+    name: "git",
+    icon: "/tech/git.webp",
+  },
+  {
+    name: "bootstrap",
+    icon: "/tech/bootstrap.webp",
+  },
+];
+
+const experiences = [
+  {
+    title: "Front end engineer",
+    company_name: "TEO",
+    icon: "",
+    iconBg: "#383E56",
+    date: "2020 -  Present",
+    points: [
+      "Self-Taught Expertise: Gained proficiency in React.JS and Next.JS through independent learning.",
+      "Successful Website Development: Designed and developed a visually appealing and user-friendly portfolio website using Next.JS.",
+      "Collaborative Design: Worked closely with stakeholders to gather feedback and iterate on the portfolio website's interactivity.",
+    ],
+  },
+  {
+    title: "Software Developer",
+    company_name: "Upwork",
+    icon: "",
+    iconBg: "#E6DEDD",
+    date: "2024 - Present",
+    points: [
+      "Self-Taught Expertise: Gained proficiency in React.JS and Next.JS through independent learning.",
+      "Successful Website Development: Designed and developed a visually appealing and user-friendly portfolio website using Next.JS.",
+      "Collaborative Design: Worked closely with stakeholders to gather feedback and iterate on the portfolio website's interactivity.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    id: 1,
+    testimonial:
+      "LinkedIn is a business and employment-focused social media platform that works through websites and mobile apps.",
+    name: "Muniba Hina",
+    image: "/socialmedia/linkedin.svg",
+    link: "https://www.linkedin.com/in/muniba-hina-22342b111/",
+  },
+  {
+    id: 4,
+    testimonial:
+      "Also do check out my UI/UX Portfolio where I have shared by design studies.",
+    name: "Muniba Hina",
+    image: "/socialmedia/portfolio.svg",
+    link: "",
+  },
+  {
+    id: 5,
+    testimonial:
+      "Also do check out my Github Profile where I have shared all my codes from basic to advanced.",
+    name: "Muniba Hina",
+    image: "/tech/github.webp",
+    link: "https://github.com/muniba-123",
+  },
+];
+
+const projects: {
+  name: string;
+  description: string;
+  tags: {
+    name: string;
+    color: string;
+  }[];
+  image: string;
+  source_code_link?: string;
+  deploy_link: string;
+  platform: "Netlify" | "Vercel" | "Web";
+}[] = [
+  {
+    name: "Sparkbright Engineering Portfolio",
+    description:
+      " Designed and developed a visually appealing and user-friendly portfolio website using Next.JS.",
+    tags: [
+      {
+        name: "next",
+        color: "red-text-gradient",
+      },
+      {
+        name: "Next UI",
+        color: "orange-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "blue-text-gradient",
+      },
+    ],
+    image: "/projectimg/sparkbright.png",
+    source_code_link: "https://github.com/muniba-123/React-Admin-DashBoard",
+    platform: "Web",
+    deploy_link: "https://sparkbright.in/",
+  },
+  {
+    name: "HooBank",
+    description:
+      "A responsive Bank HomePage showcasing different features such as various payment gateways integration, easy money transfer, advanced security, etc. It has a beautiful interface made using Tailwind CSS and React",
+    tags: [
+      {
+        name: "next",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "blue-text-gradient",
+      },
+    ],
+    image: "/projectimg/hoobank.webp",
+    source_code_link: "https://github.com/muniba-123/hoobank",
+    platform: "Vercel",
+    deploy_link: "https://hoobankbyom.netlify.app/",
+  },
+  {
+    name: "MERN Dashboard",
+    description:
+      " Designed and developed a visually appealing and user-friendly Data Analytics Dashboard using MERN",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "materialui",
+        color: "orange-text-gradient",
+      },
+      {
+        name: "MongoDB",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Express",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: "/projectimg/mern.png",
+    source_code_link: "https://github.com/muniba-123/Project_MERN-Dashboard",
+    platform: "Web",
+    deploy_link: "https://admin-frontend-r705.onrender.com/",
+  },
+  {
+    name: "Metaverse Web 3.0",
+    description:
+      "A Modern UI/UX Landing Page using Framer Motion and TailwindCSS with a feel and looks of Web 3.0",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "green-text-gradient",
+      },
+      {
+        name: "framer-motion",
+        color: "orange-text-gradient",
+      },
+    ],
+    image: "/projectimg/metaverse.png",
+    source_code_link: "https://github.com/muniba-123/Project_Metaverse",
+    platform: "Vercel",
+    deploy_link: "https://project-metaverse-beta.vercel.app/",
+  },
+  {
+    name: "Issue Tracker",
+    description:
+      "A Next.JS Full Stack Issue Tracker made using Next.Js, ShadCN UI, Prisma, mySQL with latest features like Next.JS Server Components, and Serverless features.",
+    tags: [
+      {
+        name: "next",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "green-text-gradient",
+      },
+      {
+        name: "shadCN",
+        color: "orange-text-gradient",
+      },
+      {
+        name: "mySQL",
+        color: "green-text-gradient",
+      },
+    ],
+    image: "/projectimg/issuetracker.png",
+    source_code_link: "https://github.com/muniba-123/Project_Issue-Tracker",
+    platform: "Vercel",
+    deploy_link: "https://project-issue-tracker.vercel.app/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
